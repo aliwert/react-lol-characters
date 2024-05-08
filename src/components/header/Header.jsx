f6f361db3f7f3d5e8b1e693d971a1ea9d87491b2
aliwert <154356044+aliwert@users.noreply.github.com>
2024-05-08 13:51:51 +0300

    added tailwind and changed style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,12 @@ import logo from "../../assets/logo.png";
 const Header = () => {
   return (
     <Container>
-      <a href="https://www.leagueoflegends.com/en-us/" target="_blank"><Image src={logo} width="200px" /></a>
+      <a
+        href="https://www.leagueoflegends.com/en-us/"
+        target="_blank"
+      >
+        <Image src={logo} width="200px" className="mx-auto" />
+      </a>
       <br />
       <h1 className="my-2 title bg-primary h1">League Of Legends Characters</h1>
     </Container>
